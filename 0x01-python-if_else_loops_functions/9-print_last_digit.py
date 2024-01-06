07d7d8b9f05a41e2278858c4a20b78c5ab74759d
Jinakali <mutuaericmurithi@gmail.com>
2024-01-06 17:15:12 +0300

    fix: task 9, meet requirements

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    sign = 1
+    if number < 0:
+        sign = -1
+    number = number * sign
+    print("{}".format(number % 10), end='')
     return number % 10
