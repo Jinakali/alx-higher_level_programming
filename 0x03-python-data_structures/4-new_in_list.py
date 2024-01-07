7e4a2b1ffb731e272676491c4ce0a2f9b24697d0
Jinakali <mutuaericmurithi@gmail.com>
2024-01-07 21:52:12 +0300

    fix task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,10 +4,11 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         my_list_cpy = my_list
         return my_list_cpy
-    if idx >= my_list_limit:
+    if idx >= len(my_list):
         my_list_cpy = my_list
         return my_list_cpy
     new_list = []
-    for i in range(my_list_limit):
-        new_list[i] = my_list[i]
+    for i in my_list:
+        new_list.append(i)
     new_list[idx] = element
+    return new_list
