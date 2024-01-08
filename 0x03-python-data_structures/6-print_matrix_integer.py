dbf455a0b5b5ea513499d5709054be4c89233521
Jinakali <mutuaericmurithi@gmail.com>
2024-01-08 15:35:49 +0300

    fix 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == None or matrix == [[]]:
+    if matrix is None or matrix == [[]]:
         print()
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
