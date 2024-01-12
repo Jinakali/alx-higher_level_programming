0925c4a7746e5d80d1979b695686400ff2b10e44
Jinakali <mutuaericmurithi@gmail.com>
2024-01-12 05:01:48 +0300

    fix 8 & 10

--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -2,7 +2,8 @@
 best_score = __import__('10-best_score').best_score
 
 a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
+my_dict = { }
+best_key = best_score(my_dict)
 print("Best score: {}".format(best_key))
 
 best_key = best_score(None)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
         return None
-    if key in a_dictionary is True:
+    if key in a_dictionary:
         del(a_dictionary[key])
         return a_dictionary
     else:
