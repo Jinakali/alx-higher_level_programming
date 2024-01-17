9222c487da5e972cb467076096060a4e847baacc
Jinakali <mutuaericmurithi@gmail.com>
2024-01-17 04:08:48 +0300

    task 10: more documentation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,10 +5,14 @@
  * @list:list to be checked
  *
  * Return: 0 if acyclic, 1 otherwise
+ * Description:
+ * using the floyd algorithm
+ * transverse the list 2 ways, one way must be TWICE as fast as the other
+ * the starting point of the 2 ways must also be different
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *faster_transverse;/*moves 1 node faster*/
+	listint_t *faster_transverse;
 	listint_t *normal_transverse;
 
 	if (list == NULL)
